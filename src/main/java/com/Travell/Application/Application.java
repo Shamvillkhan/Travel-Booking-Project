3d9786feba639ced8775602ca95c38ccf3d50e6e
blk-chrys-shamvil <blk.chrys.shamvil@gmail.com>
2025-07-16 20:51:45 +0530

    Navbar Created and All nav item file html created

--- src/main/java/com/Travell/Application/Application.java
+++ src/main/java/com/Travell/Application/Application.java
@@ -3,11 +3,9 @@ package com.Travell.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.Travell")
 public class Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
---
+++ src/main/java/com/Travell/Controller/HeaderController.java
@@ -0,0 +1,48 @@
+package com.Travell.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+ 
+
+@Controller
+public class HeaderController {
+	
+	@GetMapping("/index")
+	public String index(Model model) {
+		
+		System.out.println("index calling >>>>>>>>>>>>>><<<<<<<<<<<<<");
+		return "index";
+	}
+	
+	@GetMapping("/about")
+	public String about() {
+		return "about";
+	}
+	
+	@GetMapping("/packages")
+	public String packages() {
+		return "packages";
+	}
+	
+	@GetMapping("/service")
+	public String services() {
+		return "service";
+	}
+	
+	@GetMapping("/gallery")
+	public String gallery() {
+		return "gallery";
+	}
+	
+	@GetMapping("/blog")
+	public String blog() {
+		return "blogs";
+	}
+	@GetMapping("/contact")
+	public String contact() {
+		return "contact";
+	}
+	
+}
